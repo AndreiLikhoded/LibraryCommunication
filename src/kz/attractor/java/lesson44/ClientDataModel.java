e46bba6d1e8ea7639164a18013598e59a550f6f2
Andrei Likhoded <liberty102and8131186@gmail.com>
2023-02-18 06:54:52 +0600

    morning changes

--- src/kz/attractor/java/lesson44/ClientDataModel.java
+++ src/kz/attractor/java/lesson44/ClientDataModel.java
@@ -1,24 +1,41 @@
 package kz.attractor.java.lesson44;
 
 import kz.attractor.java.libraryCommunication.Client;
+import kz.attractor.java.service.ReadersService;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ClientDataModel {
-    private Client client;
+
+
+    private List<Client> client;
 
     public ClientDataModel() {
-       this.client = new Client(1, "Andrei", "Bazzzanga", "qwe@qwe", "123",
-               "https://www.shutterstock.com/image-photo/skeptic-surprised-cat-thinking-dont-260nw-1905929728.jpg");
-
+        this.client = ReadersService.readFile();
     }
 
-    public Client getClient() {
+    public List<Client> getClient() {
         return client;
     }
 
-    public void setClient(Client client) {
+    public void setClient(List<Client> client) {
         this.client = client;
     }
+
+    //    private Client client;
+//
+//    public ClientDataModel() {
+//       this.client = new Client(1, "Andrei", "Bazzzanga", "qwe@qwe", "123",
+//               "https://www.shutterstock.com/image-photo/skeptic-surprised-cat-thinking-dont-260nw-1905929728.jpg");
+//
+//    }
+//
+//    public Client getClient() {
+//        return client;
+//    }
+//
+//    public void setClient(Client client) {
+//        this.client = client;
+//    }
 }
